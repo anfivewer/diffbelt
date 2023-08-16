93b67befcf03b4f837fe5ce8005e75d31d058304
Alexander Ruliov <alexander.ruliov@gmail.com>
2023-08-16 23:21:36 +0300

    playing with clap

--- crates/diffbelt_cli/src/main.rs
+++ crates/diffbelt_cli/src/main.rs
@@ -1,3 +1,30 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command()]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Collections {
+        #[command(subcommand)]
+        command: CollectionsSubcommand,
+    },
+}
+
+#[derive(Subcommand, Debug)]
+enum CollectionsSubcommand {
+    /// Lists collections
+    List,
+    /// Alias to list
+    Ls,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("Hello {:?}!", args)
 }
