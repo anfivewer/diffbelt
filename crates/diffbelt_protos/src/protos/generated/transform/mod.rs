216759e59107811bbad08699c1e07838eddb73e9
Alexander Ruliov <alexander.ruliov@gmail.com>
2023-10-23 23:15:33 +0300

    flatbuffers protos

---
+++ crates/diffbelt_protos/src/protos/generated/transform/mod.rs
@@ -0,0 +1,1 @@
+pub mod map_filter_generated;
---
+++ crates/diffbelt_protos/src/protos/mod.rs
@@ -0,0 +1,7 @@
+mod generated;
+
+pub mod transform {
+    pub mod map_filter {
+        pub use super::super::generated::transform::map_filter_generated::*;
+    }
+}
